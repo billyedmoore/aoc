e57cce65d62edd69402c907f7756b31883578f73
Billy Moore <billyedmoore@yahoo.com>
2025-11-04 13:11:03 +0000

    Solution to 2024 day two.

--- haskell/2024/two.hs
+++ haskell/2024/two.hs
@@ -1,4 +1,4 @@
-import Data.List (sort)
+import Data.List (inits, tails)
 
 readLine :: [String] -> [Int]
 readLine = map read
@@ -6,19 +6,33 @@ readLine = map read
 readInput :: FilePath -> IO [[Int]]
 readInput fileName = fmap (map (readLine . words) . lines) (readFile fileName)
 
+-- Convert report to a diff
+getDiff :: [Int] -> [Int]
+getDiff input = zipWith (-) (safeTail input) input
+
+-- Check if a report is valid from the diff
 isDiffValid :: [Int] -> Bool
 isDiffValid diff =
   (all (> 0) diff || all (< 0) diff)
     && (all (\n -> abs n <= 3) diff && all (\n -> abs n >= 1) diff)
 
+-- Tail if tail available else []
 safeTail :: [Int] -> [Int]
 safeTail (head : tail) = tail
 safeTail [] = []
+
+-- All possible reports using the problem dampener
+allPossibleReports :: [Int] -> [[Int]]
+allPossibleReports xs = zipWith (++) (inits xs) (map safeTail (tails xs))
 
 main :: IO ()
 main = do
   inputs <- readInput "two.input"
 
-  let diffs = map (\input -> zipWith (-) (safeTail input) input) inputs
-  let result = length (filter isDiffValid diffs)
-  putStrLn ("Part One Solution " ++ show result)
+  let partOneDiffs = map getDiff inputs
+  let partOneResult = length (filter isDiffValid partOneDiffs)
+  putStrLn ("Part One Solution " ++ show partOneResult)
+
+  let partTwoDiffs = map (map getDiff . allPossibleReports) inputs
+  let partTwoResult = length $ filter (any isDiffValid) partTwoDiffs
+  putStrLn ("Part Two Solution " ++ show partTwoResult)
