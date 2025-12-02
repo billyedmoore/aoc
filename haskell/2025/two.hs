d7e1748dc9fd92c9546cd1e8734d7056210fe444
Billy Moore <billyedmoore@yahoo.com>
2025-12-02 17:09:57 +0000

    Solution to 2025 day 2 part 2.

--- haskell/2025/two.hs
+++ haskell/2025/two.hs
@@ -1,5 +1,3 @@
-import Text.Parsec (parse)
-
 data Direction = Downwards | Upwards
 
 readInput :: FilePath -> IO [String]
@@ -23,11 +21,31 @@ parsePairs :: String -> [Int]
 parsePairs str = map read (splitStr '-' str)
 
 pairsToRange :: [Int] -> [Int]
--- crash and burn otherwise -> ,idomatic I know
+-- crash and burn if its not a pair, very idomatic I know
 pairsToRange [start, end] = [start .. end]
 
 inputsToProductsIds :: [String] -> [Int]
 inputsToProductsIds = concatMap (pairsToRange . parsePairs)
+
+chunksOf :: Int -> [a] -> [[a]]
+chunksOf _ [] = []
+chunksOf n xs = take n xs : chunksOf n (drop n xs)
+
+-- factors of n but excluding n
+factors :: Int -> [Int]
+factors n
+  | n >= 2 = [x | x <- [1 .. n - 1], n `mod` x == 0]
+  | otherwise = []
+
+allEqual :: [String] -> Bool
+allEqual [] = True
+-- head is safe because [] is handled
+allEqual xs = all (\e -> e == head xs) xs
+
+isInvalidWithNRepetitions :: String -> Int -> Bool
+isInvalidWithNRepetitions s n =
+  let chunks = chunksOf n s
+   in allEqual chunks
 
 isInvalidInPartOne :: Int -> Bool
 isInvalidInPartOne i =
@@ -37,8 +55,15 @@ isInvalidInPartOne i =
            in let bck = drop (len `div` 2) iAsStr
                in even len && frt == bck
 
+isInvalidInPartTwo :: Int -> Bool
+isInvalidInPartTwo i =
+  let iAsStr = show i
+   in or [isInvalidWithNRepetitions iAsStr n | n <- factors (length iAsStr)]
+
 main :: IO ()
 main = do
   inputs <- readInput "two.input"
   let partOneSol = sum (filter isInvalidInPartOne (inputsToProductsIds inputs))
-  putStrLn ("Park One Solution " ++ show partOneSol)
+  let partTwoSol = sum (filter isInvalidInPartTwo (inputsToProductsIds inputs))
+  putStrLn ("Part One Solution " ++ show partOneSol)
+  putStrLn ("Part Two Solution " ++ show partTwoSol)
