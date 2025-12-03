abe70037256218c83de7e5a07c488539f4c99e38
Billy Moore <billyedmoore@yahoo.com>
2025-12-03 09:56:54 +0000

    Solution to 2025 day 3 part 2.

--- haskell/2025/three.hs
+++ haskell/2025/three.hs
@@ -1,4 +1,4 @@
-import Data.Char (digitToInt)
+import Data.Char (digitToInt, intToDigit)
 
 readInput :: FilePath -> IO [String]
 readInput fileName = fmap lines (readFile fileName)
@@ -13,14 +13,21 @@ getMaxAndArgMax s =
       let xAsInt = digitToInt x
        in if xAsInt > valMax then (currentI, xAsInt, currentI + 1) else (iMax, valMax, currentI + 1)
 
-largestJoltageFromBank :: String -> Int
-largestJoltageFromBank s =
-  let (firstDigitI, firstDigitVal) = getMaxAndArgMax (take (length s - 1) s)
-   in let (secondDigitI, secondDigitVal) = getMaxAndArgMax (drop (firstDigitI + 1) s)
-       in firstDigitVal * 10 + secondDigitVal
+largestJoltageFromBank :: Int -> String -> Int
+largestJoltageFromBank numToTurnOn s = read (largestJoltageFromBankInternal s numToTurnOn "")
+  where
+    largestJoltageFromBankInternal :: String -> Int -> String -> String
+    largestJoltageFromBankInternal s numToTurnOn currentString =
+      let (digitI, digitVal) = getMaxAndArgMax (take (length s - numToTurnOn + 1) s)
+       in if numToTurnOn > 1
+            then
+              largestJoltageFromBankInternal (drop (digitI + 1) s) (numToTurnOn - 1) (currentString ++ [intToDigit digitVal])
+            else currentString ++ [intToDigit digitVal]
 
 main :: IO ()
 main = do
   inputs <- readInput "three.input"
-  let partOneSol = sum (map largestJoltageFromBank inputs)
+  let partOneSol = sum (map (largestJoltageFromBank 2) inputs)
+  let partTwoSol = sum (map (largestJoltageFromBank 12) inputs)
   putStrLn ("Part One Solution " ++ show partOneSol)
+  putStrLn ("Part Two Solution " ++ show partTwoSol)
