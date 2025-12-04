e9ec1871e1ed5905b33d00344ad01adefcfc8828
Billy Moore <billyedmoore@yahoo.com>
2025-12-04 10:47:27 +0000

    Solution to 2025 day 4 part 2.

--- haskell/2025/four.hs
+++ haskell/2025/four.hs
@@ -1,4 +1,7 @@
-import Data.Map.Strict as M
+-- Neither of these are truly O(n) but they are fine
+-- for this problem
+import Data.Map.Strict qualified as M
+import Data.Set qualified as S
 
 data Location = Roll | Empty deriving (Show)
 
@@ -22,31 +25,41 @@ parseChar '.' = Empty
 parseChar '@' = Roll
 parseChar c = error ("Unexpected Char " ++ show c)
 
-foundInMap :: Maybe Location -> Bool
-foundInMap (Just Roll) = True
-foundInMap _ = False
+isPosRoll :: Maybe Location -> Bool
+isPosRoll (Just Roll) = True
+isPosRoll _ = False
 
 handlePos :: (Int, Int) -> WarehouseMap -> Bool
 handlePos (i, j) map =
-  let isCurrentPosRoll = foundInMap (M.lookup (i, j) map)
-   in let rollList =
-            if not isCurrentPosRoll
-              then []
-              else
-                [ foundInMap (M.lookup (i + i_change, j + j_change) map)
-                | i_change <- [-1 .. 1],
-                  j_change <- [-1 .. 1],
-                  i_change /= 0 || j_change /= 0
-                ]
-       in let count = length (Prelude.filter id rollList)
-           in count < 4 && isCurrentPosRoll
+  let rollList =
+        [ isPosRoll (M.lookup (i + i_change, j + j_change) map)
+        | i_change <- [-1 .. 1],
+          j_change <- [-1 .. 1],
+          i_change /= 0 || j_change /= 0
+        ]
+   in let count = length (filter id rollList)
+       in count < 4
+
+getMovedRolls :: WarehouseMap -> [(Int, Int)]
+getMovedRolls map = [(i, j) | (i, j) <- M.keys map, isPosRoll (M.lookup (i, j) map), handlePos (i, j) map]
 
 solvePartOne :: WarehouseMap -> Int
-solvePartOne map = length (Prelude.filter id [handlePos (i, j) map | (i, j) <- M.keys map])
+solvePartOne map = length (getMovedRolls map)
+
+solvePartTwo :: WarehouseMap -> Int
+solvePartTwo map = solvePartTwoInternal map 0
+  where
+    solvePartTwoInternal :: WarehouseMap -> Int -> Int
+    solvePartTwoInternal map currentCount =
+      let movedRolls = S.fromList (getMovedRolls map)
+       in let newMap = M.fromList [((i, j), v) | ((i, j), v) <- M.toList map, not (S.member (i, j) movedRolls)]
+           in if not (null movedRolls) then solvePartTwoInternal newMap (currentCount + length movedRolls) else currentCount
 
 main :: IO ()
 main = do
   inputs <- readInput "four.input"
   let map = mapFromLines inputs
   let partOneSol = solvePartOne map
+  let partTwoSol = solvePartTwo map
   putStrLn ("Part One Solution " ++ show partOneSol)
+  putStrLn ("Part Two Solution " ++ show partTwoSol)
