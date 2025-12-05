4217c2a3c0b3a160d1394bcb5de7ef7cba484e31
Billy Moore <billyedmoore@yahoo.com>
2025-12-05 14:12:21 +0000

    Solution to 2025 day 5 part 2.

--- haskell/2025/five.hs
+++ haskell/2025/five.hs
@@ -1,3 +1,5 @@
+import Data.List (sortOn)
+
 readInput :: FilePath -> IO [String]
 readInput fileName = fmap lines (readFile fileName)
 
@@ -15,13 +17,39 @@ parseRanges = map (parseRange "")
 isItemFresh :: Int -> [(Int, Int)] -> Bool
 isItemFresh item ranges = or [item >= lower && item <= upper | (lower, upper) <- ranges]
 
+tailOrEmpty :: [a] -> [a]
+tailOrEmpty [] = []
+tailOrEmpty (x : xs) = xs
+
+-- Head to be used when sure the list cannot be empty
+headAssert :: [a] -> a
+headAssert [] = error "Head called on empty list"
+headAssert (x : xs) = x
+
+-- Merge the ranges into non-colliding ranges
+mergeRanges :: [(Int, Int)] -> [(Int, Int)]
+mergeRanges ranges = mergeRangesInternal ranges []
+  where
+    mergeRangesInternal :: [(Int, Int)] -> [(Int, Int)] -> [(Int, Int)]
+    mergeRangesInternal [] processed = processed
+    mergeRangesInternal (currentRange : toProcess) [] = mergeRangesInternal toProcess [currentRange]
+    mergeRangesInternal ((currentLower, currentUpper) : toProcess) processed =
+      let (prevLower, prevUpper) = headAssert processed
+       in if currentLower <= prevUpper
+            then
+              mergeRangesInternal toProcess ((prevLower, max currentUpper prevUpper) : tailOrEmpty processed)
+            else
+              mergeRangesInternal toProcess ((currentLower, currentUpper) : processed)
+
 main :: IO ()
 main = do
   inputs <- readInput "five.input"
   let (unparsedRanges, unparsedItems) = splitInput inputs []
-  let ranges = parseRanges unparsedRanges
+  let ranges = mergeRanges (sortOn fst (parseRanges unparsedRanges))
   let items :: [Int]
       items = map read unparsedItems
 
   let partOneSol = length [item | item <- items, isItemFresh item ranges]
+  let partTwoSol = sum [(upper + 1) - lower | (lower, upper) <- ranges]
   putStrLn ("Part One Solution " ++ show partOneSol)
+  putStrLn ("Part Two Solution " ++ show partTwoSol)
