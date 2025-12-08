1ceb1283aee0a28e86f2c8c4f31f66031b9aafed
Billy Moore <billyedmoore@yahoo.com>
2025-12-08 16:12:19 +0000

    Solution to 2025 day 8 part 2.

--- haskell/2025/eight.hs
+++ haskell/2025/eight.hs
@@ -4,8 +4,13 @@ import Data.Set qualified as S
 
 type Coord = (Int, Int, Int)
 
-readInput :: FilePath -> IO [(Coord, Coord)]
-readInput fileName = fmap (getPairs . map parseCoords . lines) (readFile fileName)
+readInput :: FilePath -> IO ([(Coord, Coord)], Int)
+readInput fileName = fmap processInput (readFile fileName)
+  where
+    processInput :: String -> ([(Coord, Coord)], Int)
+    processInput s =
+      let inputLines = lines s
+       in ((getPairs . map parseCoords) inputLines, length inputLines)
 
 splitOn :: (Eq a) => a -> [a] -> [[a]]
 splitOn c s = splitOnInternal c s []
@@ -36,23 +41,45 @@ getPairs cs =
     (\p1 p2 -> compare (getDistance p1) (getDistance p2))
     [(c1, c2) | (c1 : rest) <- L.tails cs, c2 <- rest]
 
-buildCircuits :: [(Coord, Coord)] -> [S.Set Coord] -> [S.Set Coord]
-buildCircuits ((p1, p2) : pairsOther) circuits = case (L.find (S.member p1) circuits, L.find (S.member p2) circuits) of
+buildCircuitsForPartOne :: [(Coord, Coord)] -> [S.Set Coord] -> [S.Set Coord]
+buildCircuitsForPartOne ((p1, p2) : remainingPairs) circuits = case (L.find (S.member p1) circuits, L.find (S.member p2) circuits) of
   (Just set1, Just set2) ->
     if set1 == set2
-      then buildCircuits pairsOther circuits
-      else buildCircuits pairsOther (S.union set1 set2 : L.delete set1 (L.delete set2 circuits))
-  (Nothing, Just set2) -> buildCircuits pairsOther (S.insert p1 set2 : L.delete set2 circuits)
-  (Just set1, Nothing) -> buildCircuits pairsOther (S.insert p2 set1 : L.delete set1 circuits)
-  (Nothing, Nothing) -> buildCircuits pairsOther (S.fromList [p1, p2] : circuits)
-buildCircuits [] circuits = circuits
+      then buildCircuitsForPartOne remainingPairs circuits
+      else buildCircuitsForPartOne remainingPairs (S.union set1 set2 : L.delete set1 (L.delete set2 circuits))
+  (Nothing, Just set2) -> buildCircuitsForPartOne remainingPairs (S.insert p1 set2 : L.delete set2 circuits)
+  (Just set1, Nothing) -> buildCircuitsForPartOne remainingPairs (S.insert p2 set1 : L.delete set1 circuits)
+  (Nothing, Nothing) -> buildCircuitsForPartOne remainingPairs (S.fromList [p1, p2] : circuits)
+buildCircuitsForPartOne [] circuits = circuits
+
+partTwo :: Int -> [(Coord, Coord)] -> [S.Set Coord] -> Int
+partTwo n [] circuits = error "End Not Found"
+partTwo n ((p1, p2) : remainingPairs) circuits = case (L.find (S.member p1) circuits, L.find (S.member p2) circuits) of
+  (Just set1, Just set2) ->
+    if set1 == set2
+      then partTwo n remainingPairs circuits
+      else recurseIfNotFinished n remainingPairs (S.union set1 set2 : L.delete set1 (L.delete set2 circuits)) (p1, p2)
+  (Nothing, Just set2) -> recurseIfNotFinished n remainingPairs (S.insert p1 set2 : L.delete set2 circuits) (p1, p2)
+  (Just set1, Nothing) -> recurseIfNotFinished n remainingPairs (S.insert p2 set1 : L.delete set1 circuits) (p1, p2)
+  (Nothing, Nothing) -> recurseIfNotFinished n remainingPairs (S.fromList [p1, p2] : circuits) (p1, p2)
+  where
+    recurseIfNotFinished :: Int -> [(Coord, Coord)] -> [S.Set Coord] -> (Coord, Coord) -> Int
+    recurseIfNotFinished n restOfPairs newCircuits (p1, p2) = case newCircuits of
+      [circuit] ->
+        if length circuit == n
+          then (let (x1, _, _) = p1 in let (x2, _, _) = p2 in x1 * x2)
+          else partTwo n restOfPairs newCircuits
+      _ -> partTwo n restOfPairs newCircuits
 
 main :: IO ()
 main = do
-  pairs <- readInput "eight.input"
+  (pairs, nBoxes) <- readInput "eight.input"
   let topNpairs = take 1000 pairs
-  let circuitLengths = map length (buildCircuits topNpairs [])
+  let circuitLengths = map length (buildCircuitsForPartOne topNpairs [])
   let sortedCircuitLengths = L.sortBy (comparing Data.Ord.Down) circuitLengths
-  let partOneSol = product (take 3 sortedCircuitLengths)
 
+  let partOneSol = product (take 3 sortedCircuitLengths)
   putStrLn ("Part One Solution " ++ show partOneSol)
+
+  let partTwoSol = partTwo nBoxes pairs []
+  putStrLn ("Part Two Solution " ++ show partTwoSol)
